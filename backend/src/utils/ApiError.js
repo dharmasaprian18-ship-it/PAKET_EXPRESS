c935cf962ae10558e683306bb0cf592342e76a0f
dharmasaprian <dharmasaprian18@gmail.com>
2026-07-05 21:43:36 +0700

    feat: Create custom API error class

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,21 @@
+const httpStatus = require('http-status-codes');
+
+/**
+ * @class ApiError
+ * @description Custom error handler for API
+ */
+class ApiError extends Error {
+  constructor(statusCode, message, isOperational = true, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = ApiError;
